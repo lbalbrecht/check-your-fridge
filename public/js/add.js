fe2c8af17140f779ef7201e9f35aa9993e132e7e
eubank87 <eubank.ashley@gmail.com>
2021-02-02 14:24:41 -0800

    small style changes

--- public/js/add.js
+++ public/js/add.js
@@ -34,45 +34,10 @@
 //     })
 // })
 
-$(document).ready(function(){
-    // materialize methods
-    $('.datepicker').datepicker();
-    $('select').formSelect();
-    $('.sidenav').sidenav();
+$(document).ready(function () {
+  // materialize methods
+  $('.datepicker').datepicker();
+  $('select').formSelect();
+  $('.sidenav').sidenav();
 
-    // font js
-    var container = document.querySelector('.content');
-    var content = container.innerText;
-    
-    function formatContent(content, container) {
-      var contentArray = content.split(' ');
-      var formattedContent = document.createElement('div');
-      contentArray.map(word => {
-        formattedContent.appendChild(createWord(word));
-      });
-      console.log(contentArray);
-    
-      container.replaceChild(formattedContent, container.firstChild);
-    };
-    
-    function createWord(characters) {
-      var word = document.createElement('div');
-      var wordArray = characters.split('');
-      wordArray.map(char => {
-        word.appendChild(formatCharacter(char));
-      });
-      word.appendChild(formatCharacter('&nbsp;'));
-      return word;
-    }
-    
-    function formatCharacter(text) {
-      var text = text === ' ' ? '&nbsp;' : text;
-      var character = document.createElement('span');
-      character.innerHTML = text;
-      return character;
-    }
-    
-    
-    formatContent(content, container);
-
-  });
+});
